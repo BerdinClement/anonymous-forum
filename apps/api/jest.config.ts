8b5510b4be37f9932ee48b24613df9144fdaf417
Clément Berdin <clement.berdin@gmail.com>
2025-05-13 23:05:46 +0200

    fix(api): test script

---
+++ apps/api/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  rootDir: '.',
+  transform: {
+	'^.+\\.tsx?$': 'ts-jest',
+  },
+  testMatch: ['**/__tests__/**/*.spec.ts', '**/?(*.)+(spec|test).ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+};
