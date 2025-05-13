96c542c9a96b9abe1fea54366a2221ff259095c8
Clément Berdin <clement.berdin@gmail.com>
2025-05-13 21:21:12 +0200

    feat(thread): add message service

---
+++ apps/thread/lib/message/MessageService.ts
@@ -0,0 +1,15 @@
+import {Message} from "@/types/Message";
+
+class MessageService {
+  private baseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+
+  async getAllMessages(): Promise<Message[]> {
+	const res = await fetch(`${this.baseUrl}/messages`);
+	if (!res.ok) {
+	  throw new Error('Failed to fetch messages');
+	}
+	return res.json();
+  }
+}
+
+export default new MessageService();
