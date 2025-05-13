cc4273df933225fe84af8dbd3e6087bb6258785c
Clément Berdin <clement.berdin@gmail.com>
2025-05-13 23:12:12 +0200

    fix(ci): install deps

--- apps/api/jest.config.js
+++ apps/api/jest.config.js
@@ -1,9 +1,9 @@
-export default {
+module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   rootDir: '.',
   transform: {
-	'^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': 'ts-jest',
   },
   testMatch: ['**/__tests__/**/*.spec.ts', '**/?(*.)+(spec|test).ts'],
   moduleFileExtensions: ['ts', 'js', 'json'],
